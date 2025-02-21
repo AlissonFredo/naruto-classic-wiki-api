ed430d45c8ff90164ba48c00274dedc3480d0ebd
AlissonFredo <douglasalissonsf@gmail.com>
2025-02-21 20:32:56 -0300

    feat: add function to feach all characters
    - Implements an endpoint to retrieve all registered characters.

--- controllers/CharacterController.js
+++ controllers/CharacterController.js
@@ -8,8 +8,17 @@ class CharacterController {
       return res.status(201).json(character);
     } catch (error) {
       console.log(error);
-
       return res.status(500).json({ error: "Error creating character" });
+    }
+  }
+
+  async findAll(req, res) {
+    try {
+      const characters = await Character.findAll();
+      return res.status(200).json(characters);
+    } catch (error) {
+      console.log(error);
+      return res.status(500).json({ error: "Error finding all characters" });
     }
   }
 }
--- routes/characterRoutes.js
+++ routes/characterRoutes.js
@@ -4,5 +4,6 @@ const CharacterController = require("../controllers/CharacterController");
 const router = express.Router();
 
 router.post("/characters", CharacterController.create);
+router.get("/characters", CharacterController.findAll);
 
 module.exports = router;
