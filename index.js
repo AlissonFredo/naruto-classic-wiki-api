53498744b682b483ae99c73fa48d9bc153ba0a3e
AlissonFredo <douglasalissonsf@gmail.com>
2025-03-13 01:13:02 -0300

    fix: fix bug where API documentation was not loading on Vercel

--- index.js
+++ index.js
@@ -3,37 +3,33 @@ const database = require("./config/database");
 const characterRoutes = require("./routes/characterRoutes");
 const villageRoutes = require("./routes/villageRoutes");
 const cors = require("cors");
-
-const swaggerConfig = require("./config/swaggerConfig");
-
-// const swaggerUi = require("swagger-ui-express");
-// const path = require("path");
+const swaggerUi = require("swagger-ui-express");
+const path = require("path");
 
 const app = express();
-
-// app.use("/swagger.json", express.static(path.join(__dirname, "swagger.json")));
-
-// app.use("/swagger-custom.js", express.static(path.join(__dirname, "swagger-custom.js")));
-
-// const options = {
-//   swaggerOptions: {
-//     url: "/swagger.json",
-//   },
-//   customCssUrl: "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css",
-//   customJs: "/swagger-custom.js",
-// };
-
-// app.use("/", swaggerUi.serve, swaggerUi.setup(null, options));
-
 const port = 3000;
+
+app.use("/swagger.json", express.static(path.join(__dirname, "swagger.json")));
+app.use(
+  "/swagger-custom.js",
+  express.static(path.join(__dirname, "swagger-custom.js"))
+);
+
+const options = {
+  swaggerOptions: {
+    url: "/swagger.json",
+  },
+  customCssUrl:
+    "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css",
+  customJs: "/swagger-custom.js",
+};
+
+app.use("/", swaggerUi.serve, swaggerUi.setup(null, options));
 
 app.use(express.json());
 app.use(cors());
 app.use(characterRoutes);
 app.use(villageRoutes);
-
-// Configurando o Swagger
-swaggerConfig.setup(app);
 
 app.listen(port, async () => {
   await database.connect();
