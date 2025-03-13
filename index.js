833cb7b6a6911229957028557e9cca42aa9353c2
AlissonFredo <douglasalissonsf@gmail.com>
2025-03-13 00:38:16 -0300

    refactor: adjust the code to create swagger from a json file

--- index.js
+++ index.js
@@ -4,27 +4,26 @@ const characterRoutes = require("./routes/characterRoutes");
 const villageRoutes = require("./routes/villageRoutes");
 const cors = require("cors");
 
-const swaggerUi = require("swagger-ui-express");
-const path = require("path");
+const swaggerConfig = require("./config/swaggerConfig");
+
+// const swaggerUi = require("swagger-ui-express");
+// const path = require("path");
 
 const app = express();
 
-// Servindo o Swagger JSON
-app.use("/swagger.json", express.static(path.join(__dirname, "swagger.json")));
+// app.use("/swagger.json", express.static(path.join(__dirname, "swagger.json")));
 
-// Servindo o arquivo de script customizado
-app.use("/swagger-custom.js", express.static(path.join(__dirname, "swagger-custom.js")));
+// app.use("/swagger-custom.js", express.static(path.join(__dirname, "swagger-custom.js")));
 
-const options = {
-  swaggerOptions: {
-    url: "/swagger.json",
-  },
-  customCssUrl: "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css",
-  customJs: "/swagger-custom.js",
-};
+// const options = {
+//   swaggerOptions: {
+//     url: "/swagger.json",
+//   },
+//   customCssUrl: "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css",
+//   customJs: "/swagger-custom.js",
+// };
 
-// Configurando o Swagger UI
-app.use("/", swaggerUi.serve, swaggerUi.setup(null, options));
+// app.use("/", swaggerUi.serve, swaggerUi.setup(null, options));
 
 const port = 3000;
 
@@ -32,6 +31,9 @@ app.use(express.json());
 app.use(cors());
 app.use(characterRoutes);
 app.use(villageRoutes);
+
+// Configurando o Swagger
+swaggerConfig.setup(app);
 
 app.listen(port, async () => {
   await database.connect();
