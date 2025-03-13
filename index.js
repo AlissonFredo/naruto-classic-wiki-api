086ee9db800f7760ecc0511292e602282fc0b207
AlissonFredo <douglasalissonsf@gmail.com>
2025-03-13 00:28:14 -0300

    refactor: adjust the code to create swagger from a json file

--- index.js
+++ index.js
@@ -9,37 +9,22 @@ const path = require("path");
 
 const app = express();
 
-const swaggerDocument = path.join(__dirname, "swagger.json");
+// Servindo o Swagger JSON
+app.use("/swagger.json", express.static(path.join(__dirname, "swagger.json")));
 
-app.use("/swagger.json", express.static(swaggerDocument));
-
-const customCssUrl =
-  "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css";
-
+// Servindo o arquivo de script customizado
+app.use("/swagger-custom.js", express.static(path.join(__dirname, "swagger-custom.js")));
 
 const options = {
   swaggerOptions: {
-    url: "/swagger.json", 
+    url: "/swagger.json",
   },
-  customCssUrl, 
-  customJs: `
-    var script1 = document.createElement('script');
-    script1.src = 'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui-bundle.min.js';
-    document.head.appendChild(script1);
-
-    var script2 = document.createElement('script');
-    script2.src = 'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui-standalone-preset.min.js';
-    document.head.appendChild(script2);
-
-    var script3 = document.createElement('script');
-    script3.src = 'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui-init.js';
-    document.head.appendChild(script3);
-  `,
-
-  customSiteTitle: "Minha API - Documentação",
+  customCssUrl: "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css",
+  customJs: "/swagger-custom.js",
 };
 
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(null, options));
+// Configurando o Swagger UI
+app.use("/", swaggerUi.serve, swaggerUi.setup(null, options));
 
 const port = 3000;
 
